022234ef9061f50efcbb4920f8553fd4b2126871
Shubham Sarkar <ronniecse8@gmail.com>
2022-08-14 23:48:45 +0530

    Angular_Practice_Code_DataBinding_Commit_23:48_PM_14_08_2022_IST

--- my-project-basics-two/src/app/servers/servers.component.ts
+++ my-project-basics-two/src/app/servers/servers.component.ts
@@ -1,16 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: '[app-servers]',
-  template: '<app-server></app-server>' +
-    '<app-server></app-server>',      //component-Template
+  selector: 'app-servers',
+  // template: '<app-server></app-server>' +
+  //   '<app-server></app-server>',      //component-Template
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = "No Server was Created !!"
+  serverName = 'TestServer';
 
-  constructor() { }
+  constructor() {
+    setTimeout(() =>{
+        this.allowNewServer = true;
+        console.log(this.allowNewServer);
+    }, 2000)
+  }
 
   ngOnInit(): void {
   }
 
+  onCreateServer() {
+    this.serverCreationStatus = 'server was Created!!' + 'The Name is '+ this.serverName
+  }
+  onUpdateServerName(event){
+     this.serverName = (<HTMLInputElement>event.target).value;
+     console.log(this.serverName);
+  }
 }
