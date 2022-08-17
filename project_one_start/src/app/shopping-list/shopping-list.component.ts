4fcaea8476dbd691b76bacb9bf9f98412d1ea8c2
Shubham Sarkar <ronniecse8@gmail.com>
2022-08-17 20:16:56 +0530

    Angular_Practice_Code_Project_One_two_20:16_PM_17_08_2022_IST

--- project_one_start/src/app/shopping-list/shopping-list.component.ts
+++ project_one_start/src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Ingredients} from "../shared/ingredients.modal";
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  Ingredients : Ingredients[] =[
+    new Ingredients('Apple',4),
+    new Ingredients('Banana', 5)
 
+  ];
   constructor() { }
 
   ngOnInit(): void {
