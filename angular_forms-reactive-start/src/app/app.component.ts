e499862a02c3db928ebc90e82f4a797f78116066
Shubham Sarkar <ronniecse8@gmail.com>
2023-02-27 22:55:32 +0530

    Angular_DEV_forms_Complete_10:47_PM_27_03_2023_IST

--- angular_forms-reactive-start/src/app/app.component.ts
+++ angular_forms-reactive-start/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {FormGroup, FormControl, Validators, FormArray, FormBuilder} from "@angular/forms";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -9,7 +10,8 @@ import {FormGroup, FormControl, Validators, FormArray, FormBuilder} from "@angul
 export class AppComponent implements OnInit {
   genders = ['male', 'female'];
   signupForm: FormGroup;
-  forbiddenUsernames = ['Black', 'Sissy', 'Cucky']
+  forbiddenUsernames = ['Black', 'Sissy', 'Cucky'];
+  forbiddenEmail = ['blackpussy@dingo.com', 'bigsissy@cuckfest.com', 'fagbitch@fag.com']
 
   constructor(private formBuilder: FormBuilder) {
   }
@@ -18,13 +20,41 @@ export class AppComponent implements OnInit {
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
         'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
-        'email': new FormControl(null, [Validators.required, Validators.email]),
+        'email': new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmails.bind(this)),
       }),
       'gender': new FormControl('male'),
       'hobbies': new FormArray([])
-    })
-  }
+    });
 
+    // Value Changes
+    // this.signupForm.valueChanges.subscribe(
+    //   (value) => console.log(value)
+    // )
+    // Status Changes
+    // this.signupForm.statusChanges.subscribe((value) => {
+    //   console.log(value)
+    // } )
+
+    // Set Value
+    // this.signupForm.setValue({
+    //   userData: {
+    //     'username': 'Max',
+    //     'email': 'theriot@outlook.com',
+    //
+    //   },
+    //   'gender': 'male',
+    //   'hobbies': []
+    // })
+    //
+    // //Patch Value
+    // this.signupForm.patchValue({
+    //   userData: {
+    //     'username': 'Anna',
+    //     'email': 'theriot@outlook.com',
+    //
+    //   }
+    // })
+  }
   onSubmit() {
     console.log(this.signupForm)
   }
@@ -40,8 +70,22 @@ export class AppComponent implements OnInit {
 
   forbiddenNames(control: FormControl): { [s: string]: boolean } {
     if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
-      return { nameIsForbidden: true };
+      return {nameIsForbidden: true};
     }
     return null;
+  }
+
+  forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (this.forbiddenEmail.indexOf(control.value) !== -1) {
+          resolve({'emailIsForbidden': true});
+        } else {
+          resolve(null);
+        }
+      }, 1500);
+    });
+    return promise;
   }
 }
