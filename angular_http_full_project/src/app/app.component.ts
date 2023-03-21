62d200dbd0d6a2792a5fb30acb2a84695d3f5df5
Ronnie <roiecse8@gmail.com>
2023-03-21 09:51:49 -0700

    Angular Dev HTTP_Project_Half_21_03_2023_09:51_PM_IST

---
+++ angular_http_full_project/src/app/app.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {map} from 'rxjs/operators'
+import {Post} from "./post.model";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  loadedPosts: Post[] = [];
+  isFetching = false;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.fetchPosts()
+  }
+
+  onCreatePost(postData: Post) {
+    // Send Http request
+    // console.log(postData);
+    this.http.post<{name: string}>('https://angular-complete-guide-ef41a-default-rtdb.firebaseio.com/posts.json',postData).subscribe((responseData) =>
+    {
+      console.log(responseData)
+    })
+  }
+
+  onFetchPosts() {
+    // Send Http request
+    this.fetchPosts();
+  }
+
+  onClearPosts() {
+    // Send Http request
+
+  }
+
+  private fetchPosts() {
+    this.isFetching = true;
+    this.http.get<{[key:string]: Post}>('https://angular-complete-guide-ef41a-default-rtdb.firebaseio.com/posts.json')
+      .pipe(map(responseData=>{
+        const postArray : Post[] = [];
+        for(const key in responseData){
+          if(responseData.hasOwnProperty(key)){
+            postArray.push({...responseData[key],id:key})
+          }
+
+        }
+        return postArray;
+      })
+      )
+      .subscribe(posts  =>{
+      // console.log(posts)
+      this.isFetching = false;
+      this.loadedPosts = posts;
+    })
+  }
+}
