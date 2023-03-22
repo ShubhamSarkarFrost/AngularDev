1a094a1e58abac6d94eba39c461ed81eb9bc5b6c
Ronnie <roiecse8@gmail.com>
2023-03-22 08:04:40 -0700

    Angular_Dev_2023_22_03_9:45_IST_Short_Update

---
+++ angular_http_full_project/src/app/post.service.ts
@@ -0,0 +1,39 @@
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import { Post } from "./post.model";
+import {map} from "rxjs/operators";
+
+
+@Injectable({providedIn: "root"})
+export class PostService {
+  constructor(private http: HttpClient) {}
+
+  createAndStorePost(title:string, content:string){
+    //... send http request
+    const postData: Post ={title:title, content:content}
+    this.http.post<{name: string}>('https://angular-complete-guide-ef41a-default-rtdb.firebaseio.com/posts.json',postData).subscribe((responseData) =>
+    {
+      console.log(responseData)
+    })
+  }
+
+  fetchPost(){
+    //.... fetch http request
+   return this.http.get<{ [key: string]: Post }>('https://angular-complete-guide-ef41a-default-rtdb.firebaseio.com/posts.json')
+        .pipe(map(responseData => {
+            const postArray: Post[] = [];
+            for (const key in responseData) {
+              if (responseData.hasOwnProperty(key)) {
+                postArray.push({...responseData[key], id: key})
+              }
+
+            }
+            return postArray;
+          })
+        );
+    }
+
+   deletePost(){
+      return  this.http.delete('https://angular-complete-guide-ef41a-default-rtdb.firebaseio.com/posts.json')
+   }
+}
